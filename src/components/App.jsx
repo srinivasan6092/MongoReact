64f32288610123e830ffa82d3d86fb8a490700a1
Srinivasan B <Srinivasan_B@external.mckinsey.com>
2018-02-23 10:17:17 +0530

    sys change

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      header: 'React Component',
+      header: 'React Component C',
     };
   }
   render() {
